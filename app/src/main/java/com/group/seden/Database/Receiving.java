be885c0a0d78c58f9b89877bdedba0688a02a167
Jonathan Guzman <jguzman180@outlook.com>
2018-04-22 17:00:42 -0500

    tried fixing some stuff in Receiving, kinda lost but fixed errors

--- app/src/main/java/com/group/seden/Database/Receiving.java
+++ app/src/main/java/com/group/seden/Database/Receiving.java
@@ -4,11 +4,14 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.group.seden.model.Message;
 import com.group.seden.model.UserSession;
+
+import java.util.Map;
 
 public class Receiving {
 
@@ -16,17 +19,28 @@ public class Receiving {
     private Message message;
     private UserSession session;
 
-    public Message getMessage() {
-        Message msg = new Message();
-        myRef = FirebaseDatabase.getInstance().getReference().child("messages").child("startMessage");
+    public void getMessages(String uID) {
+        //final Message msg = new Message();
+        myRef = FirebaseDatabase.getInstance().getReference().child("messages").child(uID);
+
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated
-                String value = dataSnapshot.getValue(String.class);
-                System.out.printf("Value is : %s\n", value);
+                Message msg = dataSnapshot.getValue(Message.class);
+                System.out.printf("Message object is : %s\n", msg);
+
+                /*
+                msg.setIsEncrypted(true);
+                msg.setMsgText(valueRegex);
+                //TODO: regex value for 28 character string and setSenderID to that
+                //TODO: regex value for time in seconds
+                msg.setSenderID(dataSnapshot.getKey());
+                msg.setDeleteTime(timeRegex);
+                msg.setRecipientID(receiveRegex);
+                */
             }
 
             @Override
@@ -36,6 +50,6 @@ public class Receiving {
             }
         });
 
-        return msg;
-    }
+        return;
+    }// END getMessage() method
 }// END MODEL CLASS Receiving
