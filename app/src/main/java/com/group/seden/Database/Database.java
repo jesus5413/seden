f52293b8a82d539781af96f87dda5ebe86984f07
Isaac Buitrago <mightyking7@gmail.com>
2018-04-23 23:51:33 -0500

    Implemented lock and unlock account use cases and locked user account after 3 failed login attempts.

--- app/src/main/java/com/group/seden/Database/Database.java
+++ app/src/main/java/com/group/seden/Database/Database.java
@@ -1,17 +1,14 @@
 package com.group.seden.Database;
-
-
-import com.google.firebase.FirebaseNetworkException;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.group.seden.model.Message;
 import com.group.seden.model.UserSession;
 
 import java.util.HashMap;
+
+import static com.google.firebase.database.DatabaseError.DISCONNECTED;
 
 /**
  * Class to store data in the database
@@ -37,14 +34,15 @@ public class Database {
         childInfo.put("Email", email);
         childInfo.put("Password", password);
         childInfo.put("UniqueID", uID);
+        childInfo.put("AccountLocked", "false");
         mDatabase.child("users").child(userName).setValue(childInfo); // this points to the database, then to the users node and then stores a new node in the users nodes with a unique ID.
 
     }
 
     /**
      * Used to send a message to a recipient. It is assumed that all the fields of the message are set and valid.
+     * throws DatabaseException when the Database cannot process the message
      * @param message to send to a remote device, all fields must be set
-     * @return true if the message was successfully sent, otherwise throws DatabaseException
      */
     public static void sendMessage(Message message) throws DatabaseException
     {
@@ -72,6 +70,7 @@ public class Database {
 
     /**
      * Used to delete the last sent message
+     * throws DatabaseException when the Database cannot remove the child node
      */
     public static void deleteLastMessage() throws DatabaseException
     {
@@ -96,12 +95,43 @@ public class Database {
     }
 
     /**
-     *
-     * @param username
+     * Used to control access of an employee or client account by locking and unlocking the account
+     * with the specified username.
+     * throws DatabaseException when the Database cannot process the username
+     * @param username who's account needs to be locked
+     * @param lock true if the user will be locked out of their account, false otherwise
      */
-    public static void lockAccount(String username)
+    public static void controlAccountAccess(String username, boolean lock) throws DatabaseException
     {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child(username);
+
+        if(lock)
+        {
+                mDatabase.child("AccountLocked").setValue("true", new DatabaseReference.CompletionListener() {
+
+                    @Override
+                    public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                        if (databaseError != null)
+                            databaseError.toException();
+
+                    }
+                });
+        }
+
+        else
+        {
+                mDatabase.child("AccountLocked").setValue("false", new DatabaseReference.CompletionListener() {
+
+                @Override
+                public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                    if (databaseError != null)
+                        databaseError.toException();
+                }
+            });
+        }
 
     }
+
 
 }
