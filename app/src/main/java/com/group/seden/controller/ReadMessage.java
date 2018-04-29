5d4cc0c7defb70d73357d99f6f1c400febcc96aa
realitypm11 <paulmurray44mail@gmail.com>
2018-04-29 15:03:20 -0500

    Merge branch 'robbie' of https://github.com/jesus5413/seden into Paul
    # Conflicts:
    #	app/src/main/res/layout/activity_send_message.xml

--- app/src/main/java/com/group/seden/controller/ReadMessage.java
+++ app/src/main/java/com/group/seden/controller/ReadMessage.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
